cbe2e990002be0eae8978527d92985feb0ae812d
Dictor1488 <168811367+Dictor1488@users.noreply.github.com>
2026-04-19 09:25:01 +0300

    Update battle_full_stats.py

--- python/gui/mods/NicknameChanger/components/battle_full_stats.py
+++ python/gui/mods/NicknameChanger/components/battle_full_stats.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from ..settings import settings
 from ..utils import logger, override, try_imports
 from ..platoon_tracker import platoon_tracker
@@ -9,11 +11,33 @@ _HIDDEN_CLAN = u''
 
 class BattleFullStatsComponent(Component):
 
+    def __init__(self, controller):
+        super(BattleFullStatsComponent, self).__init__(controller)
+        self._aliases = {}
+        self._next_alias = 1
+
+    def _get_hidden_alias(self, item, uname):
+        key = (
+            item.get('vehicleID') or
+            item.get('accountDBID') or
+            item.get('dbID') or
+            item.get('userName') or
+            uname
+        )
+
+        alias = self._aliases.get(key)
+        if alias is None:
+            alias = u'Player %d' % self._next_alias
+            self._aliases[key] = alias
+            self._next_alias += 1
+        return alias
+
     def setup_hooks(self):
         FullStats = try_imports(
             lambda: __import__('gui.Scaleform.daapi.view.battle.shared.fullstats', fromlist=['StatsBase']).StatsBase,
             lambda: __import__('gui.Scaleform.daapi.view.battle.classic.fullstats', fromlist=['FullStats']).FullStats,
         )
+
         if FullStats is None or not hasattr(FullStats, '_makeVO'):
             logger.debug("FullStats._makeVO not available")
             return
@@ -23,23 +47,31 @@ class BattleFullStatsComponent(Component):
         @override(FullStats, '_makeVO')
         def hooked_make_vo(baseMethod, baseObject, *args, **kwargs):
             result = baseMethod(baseObject, *args, **kwargs)
+
             if not settings.enabled or not identity.has_original:
                 return result
+
             hide_all = settings.hide_all_nicknames
+
             if isinstance(result, (list, tuple)):
                 for item in result:
                     if not isinstance(item, dict):
                         continue
+
                     uname = item.get('userName', '')
+
                     if uname == identity.original_name:
                         item['userName'] = identity.new_name
                         if identity.new_clan:
                             item['clanAbbrev'] = identity.new_clan
+
                     elif platoon_tracker.is_platoon_mate(uname):
                         alias = platoon_tracker.get_alias(uname)
                         item['userName'] = alias
                         item['clanAbbrev'] = _HIDDEN_CLAN
+
                     elif hide_all and uname:
-                        item['userName'] = _HIDDEN_ALIAS
+                        item['userName'] = self._get_hidden_alias(item, uname)
                         item['clanAbbrev'] = _HIDDEN_CLAN
+
             return result
