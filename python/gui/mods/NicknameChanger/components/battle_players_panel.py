14d809b484b1791212a895f831026c4f754101ec
Dictor1488 <168811367+Dictor1488@users.noreply.github.com>
2026-04-19 09:25:14 +0300

    Update battle_players_panel.py

--- python/gui/mods/NicknameChanger/components/battle_players_panel.py
+++ python/gui/mods/NicknameChanger/components/battle_players_panel.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from ..settings import settings
 from ..utils import logger, override, try_imports
 from ..platoon_tracker import platoon_tracker
@@ -9,11 +11,33 @@ _HIDDEN_CLAN = u''
 
 class BattlePlayersPanelComponent(Component):
 
+    def __init__(self, controller):
+        super(BattlePlayersPanelComponent, self).__init__(controller)
+        self._aliases = {}
+        self._next_alias = 1
+
+    def _get_hidden_alias(self, item, uname):
+        key = (
+            item.get('vehicleID') or
+            item.get('accountDBID') or
+            item.get('dbID') or
+            item.get('userName') or
+            uname
+        )
+
+        alias = self._aliases.get(key)
+        if alias is None:
+            alias = u'Player %d' % self._next_alias
+            self._aliases[key] = alias
+            self._next_alias += 1
+        return alias
+
     def setup_hooks(self):
         PlayersPanel = try_imports(
             lambda: __import__('gui.Scaleform.daapi.view.battle.shared.players_panel', fromlist=['PlayersPanel']).PlayersPanel,
             lambda: __import__('gui.Scaleform.daapi.view.battle.classic.players_panel', fromlist=['PlayersPanel']).PlayersPanel,
         )
+
         if PlayersPanel is None or not hasattr(PlayersPanel, '_makeVO'):
             logger.debug("PlayersPanel._makeVO not available")
             return
@@ -23,9 +47,12 @@ class BattlePlayersPanelComponent(Component):
         @override(PlayersPanel, '_makeVO')
         def hooked_make_vo(baseMethod, baseObject, *args, **kwargs):
             result = baseMethod(baseObject, *args, **kwargs)
+
             if not settings.enabled or not identity.has_original:
                 return result
+
             hide_all = settings.hide_all_nicknames
+
             if isinstance(result, dict):
                 for key in ('leftScope', 'rightScope', 'left', 'right'):
                     items = result.get(key)
@@ -33,16 +60,21 @@ class BattlePlayersPanelComponent(Component):
                         for item in items:
                             if not isinstance(item, dict):
                                 continue
+
                             uname = item.get('userName', '')
+
                             if uname == identity.original_name:
                                 item['userName'] = identity.new_name
                                 if identity.new_clan:
                                     item['clanAbbrev'] = identity.new_clan
+
                             elif platoon_tracker.is_platoon_mate(uname):
                                 alias = platoon_tracker.get_alias(uname)
                                 item['userName'] = alias
                                 item['clanAbbrev'] = _HIDDEN_CLAN
+
                             elif hide_all and uname:
-                                item['userName'] = _HIDDEN_ALIAS
+                                item['userName'] = self._get_hidden_alias(item, uname)
                                 item['clanAbbrev'] = _HIDDEN_CLAN
+
             return result
